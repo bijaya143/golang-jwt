64c44e041ebb292ada8ec3cb717e740a63f58875
Bijaya Majhi <bijaya@clickandpress.com>
2023-03-31 15:10:01 +0545

    Project setup

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	routes "jwt-project/routes"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.AuthRoutes(router)
+	routes.UserRoutes(router)
+
+	router.GET("/api-1", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{"success": ""})
+	})
+}
